ba1220b72357ef15ca93c38ef8f0784b59e42092
Guillaume Perrin <gperrin01@gmail.com>
2015-06-03 22:18:33 +0100

    now showing songs and recommending albums based on the genre

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,12 @@
 class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+  end
+  def show
+    @song = Song.find(params[:id])
+    @albums = @song.albums
+    @genres = @song.genres.uniq
+    @result = []
+  end
 end
